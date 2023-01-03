c37c9d101bdc5255f23fd106b24e7e374c4ec2c5
JJBR13 <jjbrown1308@gmail.com>
2023-01-03 12:00:27 +0000

    Linked app.py to .html file

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, render_template,
+    flash, redirect, session, url_for)
+from flask_pymongo import PyMongo
+# Allows objects to be taken from DB
+from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
@@ -7,9 +13,19 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "hello world!"
+@app.route("/get_categories")
+def get_categories():
+    categories = mongo.db.task.find()
+    return render_template("categories.html", categories=categories)
 
 
 if __name__ == "__main__":
