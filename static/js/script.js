3b51a36c1fbe052b064c2020619a647730d96092
JJBR13 <jjbrown1308@gmail.com>
2023-01-04 10:08:31 +0000

    Syntax error fixed js script path & further edit to footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    console.log('im sidenav')
     $(".dropdown-trigger").dropdown({ hover: false });
+    console.log('dropdown baby')
   });
