5d26f8a74e6002c83f6d48b789cd0e5950e9db5a
JJBR13 <jjbrown1308@gmail.com>
2023-01-16 22:15:22 +0000

    All files formatted

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $(".dropdown-trigger").dropdown({ hover: false });
-    M.updateTextFields();
-    $('.carousel').carousel();
-    $('select').formSelect();
-    $('.modal').modal();
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: "right"
   });
+  $(".dropdown-trigger").dropdown({
+    hover: false
+  });
+  M.updateTextFields();
+  $('.carousel').carousel();
+  $('select').formSelect();
+  $('.modal').modal();
+});
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
